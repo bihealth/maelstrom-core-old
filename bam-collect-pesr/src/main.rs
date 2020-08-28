22ec11032d86f355c5ac288cc33ecbc5cf326ceb
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-08-28 10:33:03 +0200

    :zap: Skeelletons for evidence collection tools.

---
+++ bam-collect-pesr/src/main.rs
@@ -0,0 +1,101 @@
+/// bam-collect-pesr -- Collect paired end and split read evidence from BAM.
+use std::fs;
+use std::path::Path;
+
+use clap::{App, Arg, ArgMatches};
+use git_version::git_version;
+use log::{debug, info, LevelFilter};
+
+use lib_common::Error;
+use lib_config::Config;
+
+/// Command line options
+#[derive(Debug)]
+struct Options {
+    /// Verbosity level
+    verbosity: u64,
+    /// Path to configuration file to use,
+    path_config: Option<String>,
+    /// Path to input file.
+    path_input: String,
+    /// Path to output file.
+    path_output: String,
+    /// Overwrite output file.
+    overwrite: bool,
+}
+
+impl Options {
+    pub fn from_arg_matches<'a>(matches: &ArgMatches<'a>) -> Result<Self, Error> {
+        Ok(Options {
+            verbosity: matches.occurrences_of("v"),
+            path_config: matches.value_of("config").map(|s| s.to_string()),
+            path_input: match matches.value_of("input") {
+                Some(x) => String::from(x),
+                None => return Err(Error::OptionMissing()),
+            },
+            path_output: match matches.value_of("output") {
+                Some(x) => String::from(x),
+                None => return Err(Error::OptionMissing()),
+            },
+            overwrite: matches.occurrences_of("overwrite") > 0,
+        })
+    }
+}
+
+fn main() -> Result<(), Error> {
+    // Setup command line parser and parse options.
+    let matches = App::new("snappysv-bam-collect-doc")
+        .version(git_version!())
+        .author("Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>")
+        .about("Collect paired end and split read evidence from BAM")
+        .args(&[
+            Arg::from_usage("-v... 'Increase verbosity'"),
+            Arg::from_usage("--overwrite 'Allow overwriting of output file'"),
+            Arg::from_usage("-c, --config=[FILE] 'Sets a custom config file'"),
+            Arg::from_usage("<input> 'input file to read from'"),
+            Arg::from_usage("<output> 'output file to write to'"),
+        ])
+        .get_matches();
+    let options = Options::from_arg_matches(&matches)?;
+
+    // Output file must not exist yet.
+    if Path::new(&options.path_output).exists() && !options.overwrite {
+        return Err(Error::OutputFileExists());
+    }
+
+    // Setup logging verbosity.
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{} [{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d %H:%M:%S]"),
+                record.level(),
+                message
+            ))
+        })
+        .level(if matches.is_present("verbose") {
+            LevelFilter::Debug
+        } else {
+            LevelFilter::Info
+        })
+        .chain(std::io::stderr())
+        .apply()
+        .unwrap();
+    info!("Starting snappysv-bam-collect-doc");
+    info!("options: {:?}", &options);
+
+    // Parse further settings from configuration file.
+    let config: Config = match &options.path_config {
+        None => toml::from_str("").unwrap(),
+        Some(path_config) => {
+            debug!("Loading config file: {}", &path_config);
+            let contents = fs::read_to_string(&path_config)?;
+            toml::from_str(&contents).unwrap()
+        }
+    };
+    info!("options: {:?}", &config);
+
+    info!("All done. Have a nice day!");
+
+    Ok(())
+}
